b31372214c2a619aa73c49bfa7226632b2ed193e
madkarmaa <madkarma99@gmail.com>
2025-10-06 11:48:10 +0200

    feat: add landing screen (static for now)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="relative h-screen">
+	<img
+		src="/backgrounds/mountain-with-sky.webp"
+		alt="Mountain background with sky"
+		class="absolute h-full w-full object-cover"
+	/>
+	<h1
+		class="space-grotesk-600 absolute top-[43%] left-1/2 -translate-x-1/2 -translate-y-[43%] transform text-[10rem] text-white"
+	>
+		MadKarma
+	</h1>
+	<img
+		src="/backgrounds/mountain.webp"
+		alt="Mountain background with no sky"
+		class="absolute z-10 h-full w-full object-cover"
+	/>
+</div>
