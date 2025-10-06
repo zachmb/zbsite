e833106592c1701d9bc274de707ae095f8a453dc
madkarmaa <madkarma99@gmail.com>
2025-10-06 14:39:03 +0200

    feat: add dynamic page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import '../app.css'
-	let { children } = $props();
+	import '../app.css';
+	import { page } from '$app/state';
+	import { type Snippet } from 'svelte';
+
+	type Props = { children: Snippet };
+	let { children }: Props = $props();
 </script>
+
+<svelte:head>
+	<title>{page.url.pathname}</title>
+</svelte:head>
 
 {@render children()}
