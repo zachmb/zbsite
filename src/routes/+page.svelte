b62b5b052b53942aa358f5f179ca0434a94c30e0
madkarmaa <madkarma99@gmail.com>
2025-10-06 14:21:28 +0200

    refactor: add delay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
 	{#if showTitle}
 		<h1
 			class="space-grotesk-700 absolute top-[46.5%] left-1/2 -translate-x-1/2 -translate-y-1/2 transform px-4 whitespace-nowrap text-white mix-blend-overlay md:top-[45%] lg:top-[44%]"
-			in:slide={{ duration: 1000, easing: expoOut, axis: 'y' }}
+			in:slide={{ delay: 750, duration: 1100, easing: expoOut, axis: 'y' }}
 		>
 			MADKARMA
 		</h1>
