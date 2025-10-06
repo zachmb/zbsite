ab81f33f8ad9ab9161af1667caf50bcf46f6d91a
madkarmaa <madkarma99@gmail.com>
2025-10-06 12:07:01 +0200

    feat: add title animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { slide } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
+
+	let showTitle = $state(false);
+	onMount(() => setTimeout(() => (showTitle = true), 1));
+</script>
+
 <div class="relative h-screen overflow-hidden">
 	<img
 		src="/backgrounds/mountain-with-sky.webp"
 		alt="Mountain background with sky"
 		class="absolute h-full w-full object-cover"
 	/>
-	<h1
-		class="space-grotesk-600 absolute top-[43%] left-1/2 -translate-x-1/2 -translate-y-[43%] transform text-[10rem] text-white"
-	>
-		MadKarma
-	</h1>
+	{#if showTitle}
+		<h1
+			class="space-grotesk-600 absolute top-[43%] left-1/2 -translate-x-1/2 -translate-y-[43%] transform text-[10rem] text-white"
+			in:slide={{ duration: 1000, easing: expoOut, axis: 'y' }}
+		>
+			MadKarma
+		</h1>
+	{/if}
 	<img
 		src="/backgrounds/mountain.webp"
 		alt="Mountain background with no sky"
