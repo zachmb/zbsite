58dcc6463f69cd5a12e103fea3dd0e298f9d6754
madkarmaa <madkarma99@gmail.com>
2025-10-07 17:19:51 +0200

    feat: add FontAwesome icon component

---
+++ src/lib/components/fontawesome/Icon.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	type Props = {
+		pack:
+			| 'brands'
+			| 'chisel'
+			| 'classic'
+			| 'duotone'
+			| 'etch'
+			| 'jelly'
+			| 'notdog'
+			| 'sharp'
+			| 'sharp-duotone'
+			| 'slab'
+			| 'thumbprint'
+			| 'utility'
+			| 'whiteboard';
+		style: 'solid' | 'regular' | 'light' | 'thin' | 'semibold';
+		name: string;
+	};
+	let { pack, style, name }: Props = $props();
+	if (name.startsWith('fa-')) name = name.slice(3);
+</script>
+
+<i class={`fa-${pack} fa-${style} fa-${name}`}></i>
