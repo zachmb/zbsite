f216294ffcae1b8953c2f0ac3d84d04aaf1605d5
madkarmaa <madkarma99@gmail.com>
2025-10-07 12:12:32 +0200

    feat: make easter egg functional

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,17 +4,31 @@
 	import { page } from '$app/state';
 	import { type Snippet, onMount } from 'svelte';
 	import { generateRandomSquarePng } from '$utils';
+	import { showEasterEgg } from '$lib/stores';
 
 	type Props = { children: Snippet };
 	let { children }: Props = $props();
 
 	let iconUrl = $state('');
-	onMount(() => (iconUrl = generateRandomSquarePng(16)));
+	onMount(() => {
+		iconUrl = generateRandomSquarePng(16);
+
+		Object.defineProperty(window, '__whatCouldThisDo', {
+			get: () => ($showEasterEgg = !$showEasterEgg)
+		});
+	});
 </script>
 
 <svelte:head>
 	<title>{page.url.pathname}</title>
 	<link rel="icon" type="image/png" href={iconUrl} />
 </svelte:head>
+
+<svelte:body
+	{@attach (body) => {
+		if ($showEasterEgg) body.classList.add('minecraft');
+		else body.classList.remove('minecraft');
+	}}
+/>
 
 {@render children()}
