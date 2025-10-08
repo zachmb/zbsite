9ddc3d070a7073b0253e999fd35c0e2c8ace3aa3
madkarmaa <madkarma99@gmail.com>
2025-10-08 14:32:50 +0200

    feat: add writable store with localStorage

---
+++ src/lib/utils/rememberable.ts
@@ -0,0 +1,39 @@
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
+
+type JSONPrimitive = string | number | boolean | null | undefined;
+
+type Serializable =
+	| JSONPrimitive
+	| Serializable[]
+	| {
+			[key: string]: Serializable;
+	  };
+
+const rememberable = <T extends Serializable>(
+	key: string,
+	initial: T,
+	options: { serialize: (value: T) => string; deserialize: (value: string) => T } = {
+		serialize: JSON.stringify,
+		deserialize: JSON.parse
+	}
+) => {
+	const { subscribe, set, update } = writable(initial);
+
+	if (browser && localStorage) {
+		const storedValue = localStorage.getItem(key);
+		if (storedValue) set(options.deserialize(storedValue));
+	}
+
+	subscribe((value) => {
+		if (browser && localStorage) localStorage.setItem(key, options.serialize(value));
+	});
+
+	return {
+		subscribe,
+		set,
+		update
+	};
+};
+
+export default rememberable;
