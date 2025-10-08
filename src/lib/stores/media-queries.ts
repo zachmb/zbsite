7c99a03b29d1a569f270a7f91aab7c351490d0d4
madkarmaa <madkarma99@gmail.com>
2025-10-08 15:46:29 +0200

    fix: check for window existence

--- src/lib/stores/media-queries.ts
+++ src/lib/stores/media-queries.ts
@@ -1,14 +1,17 @@
+import { browser } from '$app/environment';
 import { readable } from 'svelte/store';
 
 // https://github.com/pearofducks/svelte-match-media/blob/master/index.js
 const setupMediaQuery = (query: string) => (set: (matches: boolean) => void) => {
-	const mediaQueryList = window.matchMedia(query);
+	if (browser && window.matchMedia) {
+		const mediaQueryList = window.matchMedia(query);
 
-	const callback = (event: MediaQueryListEvent) => set(event.matches);
-	mediaQueryList.addEventListener('change', callback);
+		const callback = (event: MediaQueryListEvent) => set(event.matches);
+		mediaQueryList.addEventListener('change', callback);
 
-	set(mediaQueryList.matches);
-	return () => mediaQueryList.removeEventListener('change', callback);
+		set(mediaQueryList.matches);
+		return () => mediaQueryList.removeEventListener('change', callback);
+	} // maybe it'll break in the future if I don't return anything here?
 };
 
-export const systemPrefersDark = readable(false, setupMediaQuery('(prefers-color-scheme: dark)'));
+export const systemPrefersDark = readable(true, setupMediaQuery('(prefers-color-scheme: dark)'));
