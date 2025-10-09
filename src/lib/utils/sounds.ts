33fc50cff396babf8eaacc49764b22ea0e7fdf04
madkarmaa <madkarma99@gmail.com>
2025-10-09 12:06:08 +0200

    fix: pause and reset audio when play() is called

--- src/lib/utils/sounds.ts
+++ src/lib/utils/sounds.ts
@@ -1,29 +1,32 @@
 import { browser } from '$app/environment';
 
 class Sound {
-	private _audio: HTMLAudioElement | null = null;
+	private audio: HTMLAudioElement | null = null;
 
 	constructor(
 		public readonly src: string,
-		private volume: number = 1
+		private readonly volume: number = 1
 	) {
 		if (browser) {
-			this._audio = new Audio(this.src);
-			this._audio.volume = this.volume.clamp(0, 1);
+			this.audio = new Audio(this.src);
+			this.audio.volume = this.volume.clamp(0, 1);
 
-			this._audio.addEventListener('pause', () => (this._audio!.currentTime = 0));
+			this.audio.addEventListener('pause', () => (this.audio!.currentTime = 0));
 		}
 	}
 
 	load() {
-		if (this._audio) {
-			this._audio.preload = 'auto';
-			this._audio.load();
+		if (this.audio) {
+			this.audio.preload = 'auto';
+			this.audio.load();
 		}
 	}
 
 	play() {
-		if (this._audio) this._audio.play();
+		if (this.audio) {
+			this.audio.pause();
+			this.audio.play();
+		}
 	}
 }
 
