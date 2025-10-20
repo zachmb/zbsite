f6c010ec473d426067a8be43ada3b84f774e0a9d
madkarmaa <madkarma99@gmail.com>
2025-10-20 17:07:31 +0200

    feat: content add sections

---
+++ src/lib/components/Section.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	type Props = { title: string; children: Snippet };
+	let { title, children }: Props = $props();
+</script>
+
+<section class="relative h-screen">
+	<h2
+		class="!text-tertiary !orbitron-700 pointer-events-none absolute top-1/2 left-1/2 translate-[-50%] text-center opacity-40 select-none"
+	>
+		{title}
+	</h2>
+	{@render children()}
+</section>
+
+<style>
+	h2 {
+		font-size: clamp(1rem, 15vw, 10rem);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,86 @@
 <script lang="ts">
+	import type { PossiblyUndefined } from '@madkarma/ts-utils/types';
 	import type { PageProps } from './$types';
 	import Landing from '$components/Landing.svelte';
+	import Section from '$components/Section.svelte';
+	import { randomIntBetween } from '$utils';
 
 	let { data }: PageProps = $props();
+
+	type PosCallback = () => { x: number; y: number };
+
+	const randomPos: PosCallback = () => {
+		const x = randomIntBetween(0, window.innerWidth - 150);
+		const y = randomIntBetween(0, window.innerHeight - 50);
+		return { x, y };
+	};
+
+	const sentences: {
+		paragraph: string;
+		button: string;
+		buttonPos: PosCallback;
+		action?: () => void;
+	}[] = [
+		{
+			paragraph: "If you're a cool person, you'll read my about me from the terminal :)",
+			button: 'Show it to me here',
+			buttonPos: () => naturalButtonPos || { x: 0, y: 0 }
+		},
+		{
+			paragraph: "Come onnnn, it's way cooler in the terminal!",
+			button: 'No, I said show me here',
+			buttonPos: randomPos
+		},
+		{
+			paragraph: 'Pleaseeeeee, just this once?',
+			button: 'I said NO!',
+			buttonPos: randomPos
+		},
+		{
+			paragraph: "Fine, you win. Here's my about me.",
+			button: 'Thank you!',
+			buttonPos: randomPos,
+			action: () => (showContent = true)
+		}
+	];
+
+	let showContent = $state(false);
+	let currentSentencesIndex = $state(0);
+	let currentSentences = $derived(sentences[currentSentencesIndex % sentences.length]);
+	let naturalButtonPos: PossiblyUndefined<ReturnType<PosCallback>> = $state(undefined);
+
+	$effect(() => currentSentences.action?.());
 </script>
 
 <main class="bg-background">
 	<Landing title="MADKARMA" />
+
+	<Section title="About me">
+		<div class="text-on-background relative flex h-full flex-col items-center justify-center gap-4">
+			<p>{currentSentences.paragraph}</p>
+			<button
+				class="cursor-pointer"
+				style:transform={naturalButtonPos
+					? `translate(${currentSentences.buttonPos().x - naturalButtonPos.x}px, ${currentSentences.buttonPos().y - naturalButtonPos.y}px)`
+					: undefined}
+				onclick={() => {
+					currentSentencesIndex++;
+				}}
+				{@attach (button) => {
+					if (!naturalButtonPos) {
+						const rect = button.getBoundingClientRect();
+						naturalButtonPos = { x: rect.left, y: rect.top };
+					}
+				}}
+			>
+				{currentSentences.button}
+			</button>
+		</div>
+	</Section>
 </main>
+
+<style>
+	button {
+		transition: transform 0.3s ease-in-out;
+	}
+</style>
