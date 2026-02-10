4882c0a37d4fd5c714ee69988dc48bb770809a83
zachbas <zachbas@Zacharys-MacBook-Air-6.local>
2026-02-10 14:03:41 -0600

    just hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,10 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import Landing from '$components/Landing.svelte';
-	import Section from '$components/Section.svelte';
-	import Holding from '$components/Holding.svelte';
-	import Seriously from '$components/Seriously.svelte';
-
-	import GlassCoral from '$components/GlassCoral.svelte';
 
 	let { data }: PageProps = $props();
 </script>
 
 <main class="bg-background">
 	<Landing title="ZACHARY" />
-
-	<GlassCoral />
-
-	<Section title="About me">
-		<div class="text-text flex h-full flex-col items-center justify-center gap-4">
-			{data.user_info.bio}
-		</div>
-	</Section>
-
-	<Seriously />
-
-	<Holding />
 </main>
